b71bcfda7e1e4b90690e10f59cd6fe67b713a3c4
Rasmus Dencker <rd@firtal.com>
2015-03-30 16:50:15 +0200

    Added service provider

---
+++ src/SimpleExpressionServiceProvider.php
@@ -0,0 +1,28 @@
+<?php  namespace Dencker\SimpleExpressions; 
+
+use Dencker\SimpleExpressions\Expressions\AndExpression;
+use Dencker\SimpleExpressions\Expressions\ContainsExpression;
+use Dencker\SimpleExpressions\Expressions\EqualsExpression;
+use Dencker\SimpleExpressions\Expressions\LiteralExpression;
+use Dencker\SimpleExpressions\Expressions\NumericExpression;
+use Dencker\SimpleExpressions\Expressions\OrExpression;
+
+class SimpleExpressionServiceProvider {
+
+    public function register()
+    {
+        ExpressionFactory::extend([
+           new NumericExpression,
+           new LiteralExpression,
+           new EqualsExpression,
+           new ContainsExpression,
+           new AndExpression,
+           new OrExpression,
+        ]);
+    }
+
+    public function boot()
+    {
+
+    }
+}
